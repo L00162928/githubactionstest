fb0755978957f45d44e5f4b329ebd5f98f0e0876
Alwyn Benny <L00162928@student.lyit.ie>
2021-03-18 11:50:22 +0000

    Initial files added

---
+++ src/main/java/githubactionstest/App.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package githubactionstest;
+
+/**
+ * @author Administrator
+ *
+ */
+public class App {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello LYIT Devops Students..!");
+	}
+
+}
